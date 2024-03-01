c104e80cddb931bf852b241249d4e8b59012f156
ssk-12 <ullegadda.srikanta@gmail.com>
2024-03-02 00:21:25 +0530

    severless be

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
@@ -8,40 +8,54 @@ const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string,
-  }
+  },
+	Variables : {
+		userId: string
+	}
 }>();
 
-app.get("/", (c) => {
+//middleware
+app.use('/api/v1/blog/*', async (c, next) => {
+	const jwt = c.req.header('Authorization');
+	if (!jwt) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const token = jwt.split(' ')[1];
+	const payload = await verify(token, c.env.JWT_SECRET);
+	if (!payload) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', payload.id);
+	await next()
+})
+
+app.get("/home", (c) => {
   return c.json({
     message: "home page"
   })
 })
 
 app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
 
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-
-  try {
-
-    const user = await prisma.user.create({
-      data:{
-        email: body.email,
-        password : body.password
-      }
-    });
-
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  
-    return c.text('jwt here')
-
-  } catch (error) {
-    return c.status(403)
-  }
-
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
 })
 
 
@@ -51,19 +65,24 @@ app.post('/api/v1/signin', async (c) => {
 	}).$extends(withAccelerate());
 
 	const body = await c.req.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			email: body.email
-		}
-	});
-
-	if (!user) {
-		c.status(403);
-		return c.json({ error: "user not found" });
-	}
-
-	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-	return c.json({ jwt });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    });
+  
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" });
+    }
+  
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (error) {
+    return c.json({ error });
+  }
 })
 
 
@@ -73,9 +92,10 @@ app.get('/api/v1/blog/:id', (c) => {
   return c.text('get blog route')
 })
 
-app.post('/api/v1/blog', (c) => {
 
-  return c.text('signin route')
+app.post('/api/v1/blog', (c) => {
+	console.log(c.get('userId'));
+	return c.text('signin route')
 })
 
 app.put('/api/v1/blog', (c) => {
